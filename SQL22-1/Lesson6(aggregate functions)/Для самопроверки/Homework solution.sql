7849a10e04ec49645aadbe657e0a63ffa4eb3c64
Alexander <aomelchenko.pro@outlook.com>
2022-05-04 03:36:52 +0300

    Add the files of the lesson 6

---
+++ SQL22-1/Lesson6(aggregate functions)/Для самопроверки/Homework solution.sql
@@ -0,0 +1,19 @@
+/*
+"Напишите запрос, который в разрезе идентификатора карты (CreditCardID) вернет: кол-во заказов, общую сумму заказов, максимальную сумму
+заказа, минимальную сумму заказа, среднюю сумму заказа. Сумма заказа - [SubTotal]. 
+Не учитывайте заказы, у которых не указан идент. карты (CreditCardID is not null)
+- Используется таблица [Sales].[SalesOrderHeader] 
+  https://dataedo.com/samples/html/AdventureWorks/doc/AdventureWorks_2/tables/Sales_SalesOrderHeader_185.html
+- Отсортировать результат по общей сумме заказа (по убыванию)"					
+
+*/
+select t1.CreditCardID,
+       count(t1.SalesOrderID) as [orderQty], -- кол-во заказов
+       sum(t1.SubTotal)       as [totalSum], -- общая сумма заказов
+	   max(t1.SubTotal)       as [maxSum],   -- максимальная сумма
+	   min(t1.SubTotal)       as [minSum],   -- минимальная сумма
+	   avg(t1.SubTotal)       as [avgSum]    -- средняя сумма
+  from [Sales].[SalesOrderHeader] as t1
+ where t1.CreditCardID is not null
+ group by t1.CreditCardID
+ order by  sum(t1.SubTotal) desc;
