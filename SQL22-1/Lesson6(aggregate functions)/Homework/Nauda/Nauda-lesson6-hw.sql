590324b330babe02998fe2ae8ba232122c678ae1
Alexander <aomelchenko.pro@outlook.com>
2022-05-08 10:31:26 +0300

    Create Nauda-lesson6-hw.sql

---
+++ SQL22-1/Lesson6(aggregate functions)/Homework/Nauda/Nauda-lesson6-hw.sql
@@ -0,0 +1,23 @@
+---===============================================ДЗ 6============================
+/* "Напишите запрос, который в разрезе идентификатора карты (CreditCardID) вернет: 
+кол-во заказов,
+общую сумму заказов,
+максимальную сумму заказа,
+минимальную сумму заказа, 
+среднюю сумму заказа. 
+Сумма заказа - [SubTotal]. Не учитывайте заказы, у которых не указан идент. карты (CreditCardID is not null)
+- Используется таблица [Sales].[SalesOrderHeader] 
+  https://dataedo.com/samples/html/AdventureWorks/doc/AdventureWorks_2/tables/Sales_SalesOrderHeader_185.html
+- Отсортировать результат по общей сумме заказа (по убыванию)"
+*/
+Use SQL221;
+Select s.CreditCardID,
+       count (Distinct s.SalesOrderID) as [CountOrder], 
+       sum(s.SubTotal) as [TotalSumOrder],
+       max(s.SubTotal) as [MaxSumOrder],
+       min(s.SubTotal) as [MinSumOrder], 
+       AVG(s.SubTotal) as [AVGSumOrder]
+ from [Sales].[SalesOrderHeader] s 
+ where CreditCardID is not null
+ Group by CreditCardID 
+ order by [TotalSumOrder] desc;
