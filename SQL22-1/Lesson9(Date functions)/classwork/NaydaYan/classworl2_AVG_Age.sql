3fc08674ff8725bb376d611b5698b182d030434a
Yan <yannauda@gmail.com>
2022-05-22 13:31:03 +0300

    Update classworl2_AVG_Age.sql

--- SQL22-1/Lesson9(Date functions)/classwork/NaydaYan/classworl2_AVG_Age.sql
+++ SQL22-1/Lesson9(Date functions)/classwork/NaydaYan/classworl2_AVG_Age.sql
@@ -1,9 +1,11 @@
-  select  distinct [Gender],
-   AVG (datediff(year,BirthDate,GETDATE())) as [Age]
-  from [HumanResources].[Employee]
-  Group by gender
+  select  [Gender],
+          AVG (datediff(year,BirthDate,GETDATE())) as [Age]
+    from [HumanResources].[Employee]
+    where [JobTitle] like N'%- WC%'
+    Group by gender
 
-  select  distinct [Gender],
-  AVG (datediff(year, BirthDate, sysdatetime()) - case when 100 * month(sysdatetime()) + day(sysdatetime()) < 100 * month(BirthDate) + day(BirthDate) then 1 else 0 end)
+  select [Gender],
+          AVG (datediff(year, BirthDate, sysdatetime()) - case when 100 * month(sysdatetime()) + day(sysdatetime()) < 100 * month(BirthDate) + day(BirthDate) then 1 else 0 end)
    from [HumanResources].[Employee]
-  Group by gender
+   where [JobTitle] like '%- WC%'
+   Group by gender
