c85b5af3c211d89eeffb355feeb52e3ae7549f9d
Alexander <aomelchenko.pro@outlook.com>
2022-05-22 13:40:36 +0300

    Update do-the-next-task.sql

--- SQL22-1/Lesson9(Date functions)/classwork/Do-the-task/do-the-next-task.sql
+++ SQL22-1/Lesson9(Date functions)/classwork/Do-the-task/do-the-next-task.sql
@@ -2,4 +2,5 @@
 select upper(e.Gender) as [gender],
        avg(datediff(year, e.BirthDate, sysdatetime()) - case when 100 * month(sysdatetime()) + day(sysdatetime()) < 100 * month(e.BirthDate) + day(e.BirthDate) then 1 else 0 end) as [averageyear]
   from [HumanResources].[Employee] e
+ where e.JobTitle like '%- WC%'
  group by upper(e.Gender);
