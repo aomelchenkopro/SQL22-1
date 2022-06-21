925e68d495cc3261bd2689326f6937d8ac4edfbb
Alexander <aomelchenko.pro@outlook.com>
2022-06-21 03:43:14 +0300

    Add homework

---
+++ SQL22-1/Lesson10(Math functions)/homework/Для самопроверки/Для самопроверки.sql
@@ -0,0 +1,20 @@
+﻿/*
+Напишите запрос, который возвращает наименование должности (в верхнем регистре и без пробелов в начале строки) 
+с наибольшим количеством работников не проводивших продажи на товары цвета Black в 2013 году
+- Используются таблицы: [HumanResources].[Employee], [Sales].[SalesOrderHeader], [Sales].[SalesOrderDetail], [Production].[Product]
+- Рез. набор данных содержит: наименование должности (в верхнем регистре и без пробелов в начале строки), кол-во работников
+*/
+
+select top 1 
+       with ties
+       upper(ltrim(t4.JobTitle))           as [jobTitle],
+       count(distinct t4.BusinessEntityID) as [EmpQty]
+  from [Production].[Product]          as t1
+ inner join [Sales].[SalesOrderDetail] as t2 on t2.ProductID = t1.ProductID
+                                            and t1.Color = N'Black'
+ inner join [Sales].[SalesOrderHeader] as t3 on t3.SalesOrderID = t2.SalesOrderID
+                                            and t3.OrderDate between '20130101' and '20131231 23:59:59'
+ right outer join [HumanResources].[Employee] as t4 on t4.BusinessEntityID = t3.SalesPersonID
+ where t3.SalesOrderID is null
+ group by upper(ltrim(t4.JobTitle))
+ order by [EmpQty] desc;
