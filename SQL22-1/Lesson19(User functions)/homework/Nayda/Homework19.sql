339d2b5bdbe50240caf4809bc0ccf34bdfac41e9
Yan <yannauda@gmail.com>
2022-09-22 11:27:24 +0300

    Create Homework19.sql

---
+++ SQL22-1/Lesson19(User functions)/homework/Nayda/Homework19.sql
@@ -0,0 +1,27 @@
+/*Реализуйте пользовательскую scalar функцию, которая по идент. работника возвращает среднее кол-во уникальных заказов в месяц.
+Функция принимает один входящий параметр - @BusinessEntityID - идент. работника. Функция возвращает NUMERIC(15,2) - среднее 
+кол-во заказов в месяц. Месяц - период - yyyyMM.
+- Используется таблица -[Sales].[SalesOrderHeader]
+- Задействуйте строковую функцию FORMAT
+- Задействуйте функцию преобразования данных CONVERT
+- Задействуйте агрегатную функцию AVG*/
+use [SQL221];
+go
+CREATE FUNCTION dbo.ReturnAVGOrder (@BusinessEntityID int)
+-- drop FUNCTION dbo.ReturnAVGOrder
+	RETURNS numeric(15,2) AS
+	 BEGIN
+		DECLARE @OrderAVG numeric(15,2);
+			SET @OrderAVG = (Select convert (numeric(15,2),AVG(r.countorder)) as asdasdasd
+                               from (
+                                     select SalesPersonID,
+                                            FORMAT(OrderDate,'yyyyMM', 'en-US') as mounth,
+	                                        Count(distinct SalesOrderID) as countorder 
+                                     from [Sales].[SalesOrderHeader]
+                                     where SalesPersonID = @BusinessEntityID
+                                     group by SalesPersonID,FORMAT(OrderDate,'yyyyMM', 'en-US')) as R)	   
+		RETURN @OrderAVG;
+	END;
+GO
+ --Check 
+ SELECT dbo.ReturnAVGOrder (279)
